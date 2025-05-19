705bee4675233f495abc34e0613b812ac5cbbd6f
Signature <signature@Signatures-MacBook-Pro.local>
2025-05-19 19:12:27 +0700

    change code app.py

--- app.py
+++ app.py
@@ -4,20 +4,16 @@ import traceback
 
 app = Flask(__name__)
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/register", methods=["POST"])
 def register():
-    if request.method == "GET":
-        return "🔒 โปรดใช้ POST ในการลงทะเบียน", 405
-
     try:
         data = request.json
         write_to_sheet(data)
         return jsonify({"message": "✅ Registered successfully"}), 200
     except Exception as e:
         print("🔥 Error in /register")
-        traceback.print_exc()
+        traceback.print_exc()  # <--- เพิ่มบรรทัดนี้!
         return jsonify({"error": str(e)}), 500
-
 
 if __name__ == "__main__":
     import os
